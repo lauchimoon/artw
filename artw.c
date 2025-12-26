9e1061f721e3473f2c1fc95fc77f4a7df92e0fe9
lauchimoon <laurentino.luna06@gmail.com>
2025-12-26 20:58:00 -0300

    Update example

--- artw.c
+++ artw.c
@@ -7,7 +7,9 @@ int main()
     Tag html_tag = tag_make(TAGTYPE_ELEMENT, "html");
     Tag body_tag = tag_make(TAGTYPE_ELEMENT, "body");
     Tag p_tag = tag_make(TAGTYPE_ELEMENT, "p");
-    Tag text = tag_make(TAGTYPE_TEXT, "Hola mundo");
+    Tag p_tag2 = tag_make(TAGTYPE_ELEMENT, "p");
+    Tag text = tag_make(TAGTYPE_TEXT, "Hello world!");
+    Tag text2 = tag_make(TAGTYPE_TEXT, "Goodbye world!");
 
     root = dtree_insert(root, html_tag);
 
@@ -16,9 +18,13 @@ int main()
 
     DOMTree body = html->nodes[html->nchild - 1];
     dtree_insert(body, p_tag);
+    dtree_insert(body, p_tag2);
 
-    DOMTree p = body->nodes[body->nchild - 1];
+    DOMTree p = body->nodes[body->nchild - 2];
     dtree_insert(p, text);
+
+    DOMTree p2 = body->nodes[body->nchild - 1];
+    dtree_insert(p2, text2);
 
     dtree_print(root);
     dtree_free(root);
